5a1d106c84262650348afa3091bcb3c6cddd72a2
Juan Neri <juanjo.neri@gmail.com>
2016-12-05 18:31:29 -0800

    add UniqueWords revision solution skeleton

---
+++ Class work 14R/UniqueWords.java
@@ -0,0 +1,28 @@
+// -----------------------------------------------------------
+// PROBLEM 1:
+// -----------------------------------------------------------
+// [PART I]
+public class UniqueWords {
+
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a sentence.");
+
+        String[] words = input.nextLine().split(" ");
+
+        // TODO: Your improved solution here!
+
+        System.out.println("There are " + count + " unique words in that sentence.");
+    }
+
+}
+
+// [PART II]
+// Asymptotic Runtime Complexity of OLD UniqueWords.java (the solution):  O(???)
+// Asymptotic Runtime Complexity of NEW UniqueWords.java (the above):     O(???)
+
+// -----------------------------------------------------------
+// PROBLEM 2:
+// -----------------------------------------------------------
+// T_1(n) = \Theta(???)
+// T_2(n) = \Theta(???)
